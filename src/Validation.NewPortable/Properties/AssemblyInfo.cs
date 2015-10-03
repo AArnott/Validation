288903f377f298e96ea10b606b82abd118087154
Andrew Arnott <andrewarnott@gmail.com>
2015-10-02 21:10:02 -0700

    Add portable assembly targeting net451+win81+wpa81
    This makes available an assembly that targets the new surface area of System.Runtime instead of mscorlib. And thus, this assembly is ready to work in the new net46+dnxcore50+uwp10 world.

---
+++ src/Validation.NewPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Validation.NewPortable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Validation.NewPortable")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
