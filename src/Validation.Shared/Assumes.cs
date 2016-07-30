710ff7152f97af5877704e2ecea21b3e9d408d02
Andrew Arnott <andrewarnott@gmail.com>
2016-07-30 11:12:50 -0700

    Get StyleCop working again

--- src/Validation.Shared/Assumes.cs
+++ src/Validation.Shared/Assumes.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
-// Licensed under the MS-PL license. See LICENSE.txt file in the project root for full license information.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
 
 namespace Validation
 {
