a6a53500573e86621f5d560ed338ae5d72d737e0
Scott Dorman <scottdorman@users.noreply.github.com>
2020-09-27 12:24:27 -0400

    Adds Requires.ValidElements (#44)
    * Adds Requires.ValidElements
    
    * Fixed the unit test to use string.Empty rather than "". The azure-pipeline build treats this as an error while it was just a warning locally.
    
    * Updating tests for better code coverage.
    
    * Added null checking for ValidElements (value parameter).
    Removed using directive grouping blank lines and set an .editorconfig value to control it.
    Added additional tests to validate null value parameter, a predicate that throws an exception, and that the message formatting is correct. Also changed the Assert.Throws to use an overload that verifies the exception parameter name.
    
    * Fixing an azure-pipelines compile error that Assert.Contains should take a StringCompare parameter.
    
    * Addressing review comments.

--- src/Validation/Requires.cs
+++ src/Validation/Requires.cs
@@ -131,7 +131,7 @@ namespace Validation
         {
             // To whoever is doing random code cleaning:
             // Consider the performance when changing the code to delegate to NotNull.
-            // In general do not chain call to another function, check first and return as earlier as possible.
+            // In general do not chain call to another function, check first and return as early as possible.
             if (value is null)
             {
                 throw new ArgumentNullException(parameterName);
@@ -154,7 +154,7 @@ namespace Validation
         {
             // To whoever is doing random code cleaning:
             // Consider the performance when changing the code to delegate to NotNull.
-            // In general do not chain call to another function, check first and return as earlier as possible.
+            // In general do not chain call to another function, check first and return as early as possible.
             if (value is null)
             {
                 throw new ArgumentNullException(parameterName);
@@ -183,7 +183,7 @@ namespace Validation
         {
             // To whoever is doing random code cleaning:
             // Consider the performance when changing the code to delegate to NotNull.
-            // In general do not chain call to another function, check first and return as earlier as possible.
+            // In general do not chain call to another function, check first and return as early as possible.
             if (values is null)
             {
                 throw new ArgumentNullException(parameterName);
@@ -215,7 +215,7 @@ namespace Validation
         {
             // To whoever is doing random code cleaning:
             // Consider the performance when changing the code to delegate to NotNull.
-            // In general do not chain call to another function, check first and return as earlier as possible.
+            // In general do not chain call to another function, check first and return as early as possible.
             if (values is null)
             {
                 throw new ArgumentNullException(parameterName);
@@ -454,6 +454,81 @@ namespace Validation
             if (!condition)
             {
                 throw new ArgumentException(PrivateErrorHelpers.Format(unformattedMessage, args), parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if <paramref name="values"/> is null,
+        /// <paramref name="predicate"/> is null, or if <paramref name="values"/> is not null
+        /// <em>and</em> has an element which does not match the given predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+        /// <param name="values">The value of the argument.</param>
+        /// <param name="predicate">The predicate used to test the elements.</param>
+        /// <param name="parameterName">The name of the parameter to include in any thrown exception.</param>
+        /// <param name="message">A message to be used in the resulting exception.</param>
+        /// <exception cref="ArgumentException">Thrown if the tested condition is false.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="predicate"/> or <paramref name="values"/> is <see langword="null"/>.</exception>
+        [DebuggerStepThrough]
+        public static void ValidElements<T>([ValidatedNotNull] IEnumerable<T> values, Predicate<T> predicate, string? parameterName, string? message)
+        {
+            // To whoever is doing random code cleaning:
+            // Consider the performance when changing the code to delegate to NotNull.
+            // In general do not chain call to another function, check first and return as early as possible.
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (T value in values)
+            {
+                if (!predicate(value))
+                {
+                    throw new ArgumentException(message, parameterName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if <paramref name="values"/> is null,
+        /// <paramref name="predicate"/> is null, or if <paramref name="values"/> is not null
+        /// <em>and</em> has an element which does not match the given predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+        /// <param name="values">The value of the argument.</param>
+        /// <param name="predicate">The predicate used to test the elements.</param>
+        /// <param name="parameterName">The name of the parameter to include in any thrown exception.</param>
+        /// <param name="unformattedMessage">The unformatted message.</param>
+        /// <param name="args">Formatting arguments.</param>
+        /// <exception cref="ArgumentException">Thrown if the tested condition is false.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="predicate"/> is <see langword="null"/>.</exception>
+        [DebuggerStepThrough]
+        public static void ValidElements<T>([ValidatedNotNull] IEnumerable<T> values, Predicate<T> predicate, string? parameterName, string unformattedMessage, params object?[] args)
+        {
+            // To whoever is doing random code cleaning:
+            // Consider the performance when changing the code to delegate to NotNull.
+            // In general do not chain call to another function, check first and return as early as possible.
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (T value in values)
+            {
+                if (!predicate(value))
+                {
+                    throw new ArgumentException(PrivateErrorHelpers.Format(unformattedMessage, args), parameterName);
+                }
             }
         }
 
--- test/Validation.Tests/RequiresTests.cs
+++ test/Validation.Tests/RequiresTests.cs
@@ -228,4 +228,24 @@ public class RequiresTests
         InvalidEnumArgumentException ex = Assert.Throws<InvalidEnumArgumentException>(() => Requires.Defined((BigEnum)0x100000000, "parameterName"));
         this.logger.WriteLine(ex.Message);
     }
+
+    [Fact]
+    public void ValidElements()
+    {
+        Assert.Throws<ArgumentNullException>("values", () => Requires.ValidElements((IEnumerable<string>)null!, x => !string.IsNullOrWhiteSpace(x), "param", "test"));
+        Assert.Throws<ArgumentNullException>("predicate", () => Requires.ValidElements(new[] { -1 }, null!, "param", "Must be greater than 0."));
+        Assert.Throws<ArgumentException>("param", () => Requires.ValidElements(new[] { -1 }, v => v > 0, "param", "Must be greater than 0."));
+        Assert.Throws<ArgumentException>("param", () => Requires.ValidElements(new[] { "a", string.Empty, "b", null }, x => !string.IsNullOrWhiteSpace(x), "param", "test"));
+        Assert.ThrowsAny<Exception>(() => Requires.ValidElements(new[] { "a", "b", "c", "d" }, x => throw new InvalidOperationException(), "param", "test"));
+        Requires.ValidElements(new[] { "a", "b", "c", "d" }, x => !string.IsNullOrWhiteSpace(x), "param", "test");
+        Requires.ValidElements(new[] { 1, 2 }, v => v > 0, "param", "Must be greater than 0.");
+
+        Assert.Throws<ArgumentNullException>("values", () => Requires.ValidElements((IEnumerable<string>)null!, x => !string.IsNullOrWhiteSpace(x), "param", "test message: {0}", "param"));
+        Assert.Throws<ArgumentNullException>("predicate", () => Requires.ValidElements(new[] { -1 }, null!, "param", "{0} must be greater than 0.", "param"));
+        Assert.Contains("param must be greater than 0.", Assert.Throws<ArgumentException>("param", () => Requires.ValidElements(new[] { -1 }, v => v > 0, "param", "{0} must be greater than 0.", "param")).Message, StringComparison.InvariantCultureIgnoreCase);
+        Assert.Contains("test message: param", Assert.Throws<ArgumentException>("param", () => Requires.ValidElements(new[] { "a", string.Empty, "b", null }, x => !string.IsNullOrWhiteSpace(x), "param", "test message: {0}", "param")).Message, StringComparison.InvariantCultureIgnoreCase);
+        Assert.ThrowsAny<Exception>(() => Requires.ValidElements(new[] { "a", "b", "c", "d" }, x => throw new InvalidOperationException(), "param", "test message: {0}", "param"));
+        Requires.ValidElements(new[] { "a", "b", "c", "d" }, x => !string.IsNullOrWhiteSpace(x), "param", "test message: {0}", "param");
+        Requires.ValidElements(new[] { 1, 2 }, v => v > 0, "param", "{0} must be greater than 0.", "param");
+    }
 }
