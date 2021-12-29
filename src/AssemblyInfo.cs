2751fb53097c0464b370ba6ab6f5d867bb6860a0
Andrew Arnott <andrewarnott@live.com>
2021-12-29 08:09:25 -0700

    Apply Library.Template

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+#if NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER
+using System.Runtime.InteropServices;
+
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+#endif
